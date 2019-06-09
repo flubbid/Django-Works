f6f76659fe1e93976862332d90787935369077cd
flubbid <brendanwilson1@gmail.com>
2019-06-09 15:08:48 -0500

    addes loops to determine the number of words and alike words in the inputted text

--- wordcount-project/wordcount/views.py
+++ wordcount-project/wordcount/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def homepage(request):
     return render(request, 'home.html')
 
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+    wordlist = fulltext.split()
+    word_dictionary = {}
+    for word in wordlist:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+    sorted_words = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+    return render(request, 'count.html', {'fulltext': fulltext, "count": len(wordlist), "sorted_words": sorted_words})
